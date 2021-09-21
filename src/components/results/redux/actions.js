f7968fed0e0d879576ddf478215419f8d6992ffd
Sara Pereira <sarapereira0420@gmail.com>
2021-09-20 21:12:58 -0700

    disable loader when an error occurs

--- src/components/results/redux/actions.js
+++ src/components/results/redux/actions.js
@@ -1,4 +1,4 @@
-import { showLoading, hideLoading } from 'react-redux-loading-bar'
+import { showLoading, hideLoading } from "react-redux-loading-bar";
 import { getRecommendedKoms } from "../../common/api/recommendedKomsApi";
 import * as SearchQuerySelectors from "../../header/redux/selectors";
 
@@ -11,8 +11,7 @@ export const setRecommendedKoms = (payload) => ({
 
 export function fetchRecommendedKoms() {
   return function fetchRecommendedKomsThunk(dispatch, getState) {
-
-    dispatch(showLoading())
+    dispatch(showLoading());
 
     const state = getState();
     const watts = SearchQuerySelectors.selectWatts(state);
@@ -23,9 +22,13 @@ export function fetchRecommendedKoms() {
       address: address,
     };
 
-    return getRecommendedKoms(params).then((data) => {
-      dispatch(setRecommendedKoms(data));
-      dispatch(hideLoading())
-    });
+    return getRecommendedKoms(params)
+      .then((data) => {
+        dispatch(setRecommendedKoms(data));
+        dispatch(hideLoading());
+      })
+      .catch(() => {
+        dispatch(hideLoading());
+      });
   };
 }
